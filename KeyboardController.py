14a7cdf18375955dadc7c736000d8e9c86fb12fc
Parnkot <100835002+PARNKOT@users.noreply.github.com>
2022-11-15 18:20:00 +0300

    Update KeyboardController.py

--- KeyboardController.py
+++ KeyboardController.py
@@ -1,17 +1,20 @@
+import time
 import typing
 import keyboard
 
 
 class KeyMap:
     def __init__(self, key: str, func_when_pressed: typing.Callable,
-                 func_when_released: typing.Callable):
+                 func_when_released: typing.Callable, repeat_time=None):
         self.key = key
         self.func_when_pressed = func_when_pressed
         self.func_when_released = func_when_released
         self.is_pressed = False
+        self.is_released = True
+        self.repeat_time = repeat_time
 
-        keyboard.on_press_key(self.key, self.call_when_pressed)
-        keyboard.on_release_key(self.key, self.call_when_released)
+        keyboard.on_press_key(self.key, self.call_when_pressed, suppress=True)
+        keyboard.on_release_key(self.key, self.call_when_released, suppress=True)
 
     def call_when_pressed(self, event, *args):
         result = None
@@ -31,13 +34,13 @@ class KeyMap:
         return self.key == other.key
 
 
-class KeyboardController():
+class KeyboardController:
     def __init__(self):
         self.__key_maps = []
         self.is_loop_started = True
-        keyboard.add_hotkey("ctrl+c", self.turn_loop)
+        keyboard.add_hotkey("ctrl+c", self.switch_loop)
 
-    def turn_loop(self):
+    def switch_loop(self):
         self.is_loop_started = not self.is_loop_started
 
     def add_key_map(self, key: str, func_pressed: typing.Callable,
@@ -47,3 +50,12 @@ class KeyboardController():
     def loop(self):
         while self.is_loop_started:
             pass
+
+
+if __name__ == "__main__":
+    #keyboard.add_hotkey("w+d", lambda: print("w+d is pressed"))
+    keyboard.hook(lambda x: print("w+d is pressed", x), True)
+    while True:
+        pass
+    #controller = KeyboardController()
+    #controller.add_key_map('w+', onvif_abs_move.increase_tilt, lambda: print(""))
