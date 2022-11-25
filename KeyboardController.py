4ab9e8270ba46999b6295287e4e5ae7f787a79c0
Parnkot <100835002+PARNKOT@users.noreply.github.com>
2022-11-25 14:32:55 +0300

    Update KeyboardController.py

--- KeyboardController.py
+++ KeyboardController.py
@@ -5,23 +5,34 @@ import keyboard
 
 class KeyMap:
     def __init__(self, key: str, func_when_pressed: typing.Callable,
-                 func_when_released: typing.Callable, repeat_time=None):
+                 func_when_released: typing.Callable, repeat_time=None,
+                 once_call_press=True, once_call_release=True):
         self.key = key
-        self.func_when_pressed = func_when_pressed
-        self.func_when_released = func_when_released
+        self.func_when_pressed = func_when_pressed if func_when_pressed else self.stub
+        self.func_when_released = func_when_released if func_when_released else self.stub
+        self.once_call_press = once_call_press
+        self.once_call_release = once_call_release
         self.is_pressed = False
-        self.is_released = True
         self.repeat_time = repeat_time
 
         keyboard.on_press_key(self.key, self.call_when_pressed, suppress=True)
         keyboard.on_release_key(self.key, self.call_when_released, suppress=True)
 
+
+    @staticmethod
+    def stub(*args):
+        pass
+
     def call_when_pressed(self, event, *args):
         result = None
-        if not self.is_pressed:
+        if not self.is_pressed and self.once_call_press:
             result = self.func_when_pressed(*args)
             self.is_pressed = True
-        return result
+            return result
+        elif not self.once_call_press:
+            result = self.func_when_pressed(*args)
+            self.is_pressed = True
+            return result
 
     def call_when_released(self, event, *args):
         result = None
@@ -43,9 +54,11 @@ class KeyboardController:
     def switch_loop(self):
         self.is_loop_started = not self.is_loop_started
 
-    def add_key_map(self, key: str, func_pressed: typing.Callable,
-                    func_released: typing.Callable):
-        self.__key_maps.append(KeyMap(key, func_pressed, func_released))
+    def add_key_map(self, key: str,
+                    func_pressed: typing.Optional[typing.Callable],
+                    func_released: typing.Optional[typing.Callable],
+                    once_call=True):
+        self.__key_maps.append(KeyMap(key, func_pressed, func_released, once_call_press=once_call))
 
     def loop(self):
         while self.is_loop_started:
@@ -54,8 +67,10 @@ class KeyboardController:
 
 if __name__ == "__main__":
     #keyboard.add_hotkey("w+d", lambda: print("w+d is pressed"))
-    keyboard.hook(lambda x: print("w+d is pressed", x), True)
-    while True:
-        pass
-    #controller = KeyboardController()
-    #controller.add_key_map('w+', onvif_abs_move.increase_tilt, lambda: print(""))
+    #keyboard.add_hotkey("w+d", lambda: print("w+d is pressed"), suppress=True)
+    #keyboard.hook(lambda x: print("w+d is pressed", x), True)
+    #while True:
+    #    pass
+    controller = KeyboardController()
+    controller.add_key_map('w', lambda: print("w pressed :)"), None)
+    controller.loop()
